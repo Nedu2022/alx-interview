71278b2b7f191aa65fc29de186e2aea5319a6d68
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-06-30 15:50:40 +0100

    Pascals triangle success

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-"""Pascal Triangle Interview Challenge"""
-
-
+""" Pascal triangle """
 def pascal_triangle(n):
-    """returns a list of lists of numbers
-    representing the pascal triangle"""
-    if n <= 0:
-        return []
-
-    pascal_triangle = [0] * n
-
+    list1 = []
     for i in range(n):
-        # define a row and fill first and last idx with 1
-        new_row = [0] * (i+1)
-        new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
+        temp_list = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(list1[i-1][j-1] + list1[i-1][j])
+        list1.append(temp_list)
 
-        for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
-                new_row[j] = a + b
-
-        pascal_triangle[i] = new_row
-
-    return pascal_triangle
+    return list1
