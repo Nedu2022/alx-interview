986907686ea7564089c9ba984fe06bc1a152870b
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-07-06 21:56:48 +0100

    python task

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-""" Pascal triangle """
-def pascal_triangle(n):
-    list1 = []
-    for i in range(n):
-        temp_list = []
-        for j in range(i+1):
-            if j == 0 or j == i:
-                temp_list.append(1)
-            else:
-                temp_list.append(list1[i-1][j-1] + list1[i-1][j])
-        list1.append(temp_list)
+"""Pascal Triangle Interview Challenge"""
 
-    return list1
+
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
