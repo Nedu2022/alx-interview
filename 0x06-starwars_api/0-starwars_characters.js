1b1a3ec10757853f1a6a4ff9d81486b65550568a
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-08-11 02:21:30 +0100

    🚀🎉feat: Worked on the Star Wars API project

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,28 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
   }
-);
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+  }
+  );
+  Promise.all(promises).then((names) => {
+    names.forEach((name) => {
+      console.log(name);
+    });
+  }
+  );
+});
